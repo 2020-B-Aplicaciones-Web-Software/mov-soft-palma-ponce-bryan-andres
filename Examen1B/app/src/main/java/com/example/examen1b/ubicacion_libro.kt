b5456362df65608182e2a22fb7e5967c2a8bc0e4
Andrés <bryan.palma02@epn.edu.ec>
2021-09-26 18:21:48 -0500

    examen 2B

---
+++ Examen1B/app/src/main/java/com/example/examen1b/ubicacion_libro.kt
@@ -0,0 +1,11 @@
+package com.example.examen1b
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ubicacion_libro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ubicacion_libro)
+    }
+}
